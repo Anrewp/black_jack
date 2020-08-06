771c5461b8e26a859945fee2224b373d1475a894
Andrey Postupalenko <andrey.postupalenko@saltpepper.ru>
2020-08-06 09:00:02 +0300

    Add Bank

---
+++ bank.rb
@@ -0,0 +1,20 @@
+class Bank
+  DEFAULT_BANK_SUM = 100
+  BET = 10
+
+  @@bet_sum = 0
+  attr_reader :sum
+
+  def initialize
+    @sum = DEFAULT_BANK_SUM
+  end
+
+  def do_bet
+    @sum -= BET
+    @@bet_sum += BET
+  end
+
+  def self.bet_sum
+    @@bet_sum
+  end
+end
--- testing.rb
+++ testing.rb
@@ -1,11 +1,12 @@
 require './card.rb'
 require './player.rb'
+require './bank.rb'
 
 def test(test_number, condition, msg = '')
   puts condition ? "#{test_number}  |TRUE |  #{msg}" : "#{test_number} [[FALSE]] #{msg}"
 end
 
-#T1 
+# T1 CARD AND HAND VALUE
 c1 = Card.new(:clubs, 10)
 c2 = Card.new(:clubs, 'K')
 c3 = Card.new(:clubs, 'A')
@@ -18,5 +19,16 @@ p2 = Player.new
 p2 << c2
 p2 << c3
 
-test 1, p1.hand_value == 21, 'Check hend value count'
-test 1, p2.hand_value == 21, 'Check hend value count'
+test 1, p1.hand_value == 21, 'hend value count'
+test 1, p2.hand_value == 21, 'hend value count'
+
+# T2 BANK
+
+b1 = Bank.new
+b2 = Bank.new
+
+b1.do_bet
+b2.do_bet
+
+test 2, Bank.bet_sum == 20, 'Bet sum'
+test 2, b1.sum == 90, 'Bank sum'
